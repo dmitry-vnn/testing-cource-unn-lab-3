9852c0aa8d1e993bdd0875e390bfd9c128647aa9
Dmitry <xtopz1337@gmail.com>
2024-04-09 10:08:16 +0300

    add: tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,82 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2024 Smirnov Leonid
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <cstdint>
+
+#include <chrono> // NOLINT [build/c++11]
+#include <thread> // NOLINT [build/c++11]
+
 #include "TimedDoor.h"
+
+class MockTimerClient : public TimerClient {
+ public:
+    MOCK_METHOD(void, Timeout, (), (override));
+};
+
+class TimedDoorTest : public ::testing::Test {
+ protected:
+    TimedDoor door;
+    MockTimerClient mockClient;
+    Timer timer;
+
+    TimedDoorTest() : door(1), timer() {}
+
+    void SetUp() override {
+        timer.tregister(door.getTimeOut(), &mockClient);
+    }
+
+    void TearDown() override {
+        testing::Mock::VerifyAndClearExpectations(&mockClient);
+    }
+};
+
+TEST_F(TimedDoorTest, Timeout) {
+    door.unlock();
+    std::this_thread::sleep_for(std::chrono::seconds(door.getTimeOut()));
+    EXPECT_THROW(door.throwState(), std::runtime_error);
+}
+
+TEST_F(TimedDoorTest, LockUnlock) {
+    std::this_thread::sleep_for(std::chrono::seconds(door.getTimeOut() + 1));
+    EXPECT_NO_THROW(door.throwState());
+}
+
+TEST_F(TimedDoorTest, LockBeforeTimeout) {
+    std::this_thread::sleep_for(std::chrono::seconds(door.getTimeOut() + 1));
+    door.unlock();
+    EXPECT_THROW(door.throwState(), std::runtime_error);
+}
+
+TEST_F(TimedDoorTest, UnlockMth) {
+    door.unlock();
+    EXPECT_TRUE(door.isDoorOpened());
+}
+
+TEST_F(TimedDoorTest, LockMth) {
+    door.unlock();
+    door.lock();
+    EXPECT_FALSE(door.isDoorOpened());
+}
+
+TEST_F(TimedDoorTest, ClosedDoor) {
+    EXPECT_FALSE(door.isDoorOpened());
+}
+
+TEST_F(TimedDoorTest, UnlockThrow) {
+    door.unlock();
+    EXPECT_THROW(door.unlock(), std::logic_error);
+}
+
+TEST_F(TimedDoorTest, ThrowState) {
+    door.unlock();
+    EXPECT_THROW(door.throwState(), std::runtime_error);
+}
+
+TEST_F(TimedDoorTest, CloseBefore) {
+    EXPECT_NO_THROW(door.throwState());
+}
+
+TEST_F(TimedDoorTest, LockDtw) {
+    EXPECT_THROW(door.lock(), std::logic_error);
+}
